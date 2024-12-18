b2e72ba23bf1f83cd6009ebe37ecf8c91dad0b0e
moyashi170607 <takayaokada0607@gmail.com>
2024-12-18 15:45:25 +0900

    生命の基盤となるクラス

---
+++ src/life/BaseLife.ts
@@ -0,0 +1,43 @@
+import { GridManager } from "../module/GridManager";
+import { setting } from "../setting";
+
+export class BaseLife extends Phaser.GameObjects.Sprite{
+    grid:number[];
+
+    constructor(scene:Phaser.Scene,texture:string,gridX:number,gridY:number){
+        let x:number = setting.startPosition.x + gridX*setting.grid.width;
+        let y:number = setting.startPosition.y + gridY*setting.grid.height;
+
+        super(scene,x,y,texture);
+
+        this.grid = [gridX,gridY];
+    }
+
+    //各生物がこのターン行う内容を規定する
+    //派生クラスでオーバーライドしてください。
+    action():void{
+        alert("acrion関数はオーバーライドされる必要があります。")
+    }
+
+    //周囲１マスの生物を取得する
+    getSurroundingGrid():number[]{
+        let surroundingGrid:number[] = new Array(8);
+
+        const RESEARCH_GRID_LIST:number[][] = [
+            [-1,-1],
+            [0,-1],
+            [1,-1],
+            [-1,0],
+            [1,0],
+            [-1,1],
+            [0,1],
+            [1,1],
+        ]
+
+        for(let i=0; i<8; i++){
+            surroundingGrid[i] = GridManager.getGridElement(this.grid[0] + RESEARCH_GRID_LIST[i][0],this.grid[1] + RESEARCH_GRID_LIST[i][1]);
+        }
+
+        return surroundingGrid;
+    }
+}
